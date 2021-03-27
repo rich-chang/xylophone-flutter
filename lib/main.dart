b15100f15501929afb94a858e91d3350a931dfab
Rich <4301176+rich-chang@users.noreply.github.com>
2021-03-27 13:30:57 +0800

    Add seven flat button and play sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,67 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              FlatButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                color: Colors.teal,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                color: Colors.blue,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                color: Colors.purple,
+              ),
+            ],
+          ),
         ),
       ),
     );
